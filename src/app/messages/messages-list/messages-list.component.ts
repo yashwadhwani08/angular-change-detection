a9cde409b6e00a0775d92789923c81005fa6111d
Yash Wadhwani <yashwadhwani08@gmail.com>
2025-07-31 16:02:16 +0530

    simplified version of manual change detection using async pipe in template

--- src/app/messages/messages-list/messages-list.component.ts
+++ src/app/messages/messages-list/messages-list.component.ts
@@ -8,33 +8,37 @@ import {
   OnInit,
 } from '@angular/core';
 import { MessagesService } from '../messages.service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-messages-list',
   standalone: true,
+  imports: [AsyncPipe],
   templateUrl: './messages-list.component.html',
   styleUrl: './messages-list.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MessagesListComponent implements OnInit {
+// export class MessagesListComponent implements OnInit {
+export class MessagesListComponent {
   // messages = input.required<string[]>();
 
   private messagesService = inject(MessagesService);
-  private cdRef = inject(ChangeDetectorRef);
-  private destroyRef = inject(DestroyRef);
+  messages$ = this.messagesService.messages$;
+  // private cdRef = inject(ChangeDetectorRef);
+  // private destroyRef = inject(DestroyRef);
 
-  messages: string[] = [];
+  // messages: string[] = [];
 
-  ngOnInit(): void {
-    const subscription = this.messagesService.messages$.subscribe(
-      (messages) => {
-        this.messages = messages;
-        this.cdRef.markForCheck();
-      }
-    );
+  // ngOnInit(): void {
+  //   const subscription = this.messagesService.messages$.subscribe(
+  //     (messages) => {
+  //       this.messages = messages;
+  //       this.cdRef.markForCheck();
+  //     }
+  //   );
 
-    this.destroyRef.onDestroy(() => subscription.unsubscribe());
-  }
+  //   this.destroyRef.onDestroy(() => subscription.unsubscribe());
+  // }
 
   get debugOutput() {
     console.log('[MessagesList] "debugOutput" binding re-evaluated.');
