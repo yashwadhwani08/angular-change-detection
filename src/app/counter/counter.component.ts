316e648690348998486d62c063127a685ae6088e
Yash Wadhwani <yashwadhwani08@gmail.com>
2025-07-31 11:52:33 +0530

    changing the change-detection strategy to OnPush IMP

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -1,4 +1,11 @@
-import { Component, NgZone, OnInit, inject, signal } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  NgZone,
+  OnInit,
+  inject,
+  signal,
+} from '@angular/core';
 
 import { InfoMessageComponent } from '../info-message/info-message.component';
 
@@ -8,6 +15,7 @@ import { InfoMessageComponent } from '../info-message/info-message.component';
   templateUrl: './counter.component.html',
   styleUrl: './counter.component.css',
   imports: [InfoMessageComponent],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CounterComponent implements OnInit {
   private zone = inject(NgZone);
